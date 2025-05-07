f9e25ccf31d552f927f285a9913a42b29a5a9702
woonwoo <qufgkswkfl3@gmail.com>
2025-05-07 17:13:53 +0900

    feat: support auto-linking for URLs and line breaks in description

--- src/utils/linkify.js
+++ src/utils/linkify.js
@@ -2,6 +2,8 @@ const escapeRegExp = (string) => string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
 const linkifyDescription = (text, people) => {
 	let result = text;
+
+	// 1. 인물 이름 링크 처리
 	people.forEach((person) => {
 		const escapedName = escapeRegExp(person.name);
 		const regex = new RegExp(`(${escapedName})`, "g");
@@ -14,6 +16,16 @@ const linkifyDescription = (text, people) => {
 			`<span class="link-name" onclick="window.location='${link}'">$1</span>`
 		);
 	});
+
+	// 2. URL 자동 링크 처리
+	result = result.replace(
+		/(https?:\/\/[^\s]+)/g,
+		'<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>'
+	);
+
+	// 3. 줄바꿈 처리
+	result = result.replace(/\n/g, "<br />");
+
 	return result;
 };
 
